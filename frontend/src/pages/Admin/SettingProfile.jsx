950cf6ff75f475cd1288acf7a73be0e02f1f4b3c
Laxman67 <laxmanhenricks94@gmail.com>
2024-08-15 19:10:06 +0530

    setting profile

--- frontend/src/pages/Admin/SettingProfile.jsx
+++ frontend/src/pages/Admin/SettingProfile.jsx
@@ -1,7 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import SideBar from "../Admin/Sidebar";
+import axios from "axios";
+
+import {
+     ProfileContainer,
+     SidebarContainer,
+     Content,
+     ProfileHeader,
+     ProfileDetails,
+     ProfileLabel,
+     ProfileInfo,
+     EditButton,
+     ProfileDetail,
+     Label,
+     Value,
+} from "../../styles/SettingsProfileStyles";
+import Sidebar from "../Admin/Sidebar";
 
 const SettingProfile = () => {
-  return <div>SettingProfile</div>;
+     return (
+          <ProfileContainer>
+               <SidebarContainer>
+                    <Sidebar />
+               </SidebarContainer>
+
+               <Content>
+                    <ProfileHeader>Settings </ProfileHeader>
+               </Content>
+          </ProfileContainer>
+     );
 };
 
 export default SettingProfile;
