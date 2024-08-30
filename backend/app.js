9ca81308bddb74b3b3df5dc743ead4202dc2dba8
laxman67 <laxmanhenricks94@gmail.com>
2024-08-31 01:19:01 +0530

    env setup

--- backend/app.js
+++ backend/app.js
@@ -11,59 +11,38 @@ import attendanceRouter from './routes/attendanceRoute.js';
 import classRouter from './routes/ClassRoute.js';
 import examRouter from './routes/examRoute.js';
 import teacherRouter from './routes/teacherRoute.js';
+
 const app = express();
 configDotenv();
 
 // Database
 DBConnect();
 
-// middleware
-// This defines which site or url can request to this server & accepted methods
-
-app.use(express.json()); // To parse JSON body
-app.use(express.urlencoded({ extended: true }));
+// CORS Configuration
 app.use(
   cors({
     origin: [
-      'http://localhost:5173',
-      'https://schoolcool2-frontend.onrender.com/',
+      'http://localhost:5173', // Local development URL
+      'https://schoolcool2-frontend.onrender.com', // Production frontend URL
     ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // If you need to handle cookies or authentication headers
   })
 );
-// app.use(
 
-//   cors({
-//     origin: ['http://localhost:5173/'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   })
-// );
-// Port URL ->REST API
+// Middleware
+app.use(express.json()); // To parse JSON body
+app.use(express.urlencoded({ extended: true }));
 
-// Student
+// API Routes
 app.use('/api/v1/students', studentRouter);
-
-// Events
 app.use('/api/v1/events', eventsRouter);
-
-// library
 app.use('/api/v1/library', libraryRouter);
-
-// annoucements
 app.use('/api/v1/announcement', announcementRouter);
-
-// assignments
 app.use('/api/v1/assignment', assignmentRouter);
-
-// attendance
 app.use('/api/v1/attendance', attendanceRouter);
-
-// class
 app.use('/api/v1/classes', classRouter);
-
-// exam
 app.use('/api/v1/exam', examRouter);
-
-// teacher
 app.use('/api/v1/teachers', teacherRouter);
 
 export default app;
